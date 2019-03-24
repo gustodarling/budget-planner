32d1609bcee2265eadc38dc5ab0447f2a45a6ec0
GUSTO <gustodarling@gmail.com>
2019-03-24 13:01:20 -0700

    quick fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-process.env.NODE_ENV = process.envv.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 if (process.env.NODE_ENV === 'development') {
   require('dotenv').config({ path: '.env.development' })
