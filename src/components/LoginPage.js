61feaf61a5e395d25d0519614a63875b1efcc1d0
GUSTO <gustodarling@gmail.com>
2019-04-04 16:20:23 -0700

    styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,12 @@ import { connect } from 'react-redux'
 import { startLogin } from '../actions/auth'
 
 export const LoginPage = ({ startLogin }) => (
-  <div>
-    <button onClick={startLogin}>Login</button>
+  <div className='box-layout'>
+    <div className='box-layout__box'>
+      <h1 className='box-layout__title'>Expense App</h1>
+      <p>A coding showcase for using React, Routers, Redux, and Firebase.</p>
+      <button className='button' onClick={startLogin}>Google Sign In</button>
+    </div>
   </div>
 )
 
